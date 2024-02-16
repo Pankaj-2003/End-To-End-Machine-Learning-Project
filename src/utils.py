56e96ffda9cb85ceeae776452fcdbaa1f31a9caf
Pankaj-2003 <pankajtiwaribs@gmail.com>
2024-02-16 17:55:22 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-from exception import CustomException
+from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -44,6 +44,12 @@ def evaluate_model(X_train,y_train, X_test,y_test,models,param):
     
     except Exception as e:
         raise CustomException(e,sys)
-    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)    
     
     
